f87b776c947748d05772db9f5cf164fd241c3727
arthurhardman <arthur.augusto2012@gmail.com>
2025-05-11 16:45:08 -0300

    teste

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Configuração do CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000' || 'https://controle-financeiro-front-7egq.vercel.app/',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
